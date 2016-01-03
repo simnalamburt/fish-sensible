4eedf69edc7fad2a91093ad16371bdce37287990
Hyeon Kim <simnalamburt@gmail.com>
2016-01-03 15:51:18 +0900

    :tada: Initial commit

---
+++ init.fish
@@ -0,0 +1,12 @@
+alias l='ls -lah'
+
+alias ...='cd ../..'
+alias ....='cd ../../..'
+alias .....='cd ../../../..'
+alias ......='cd ../../../../..'
+
+alias md='mkdir -p'
+alias rd=rmdir
+
+alias mv='mv -i'
+alias cp='cp -i'
