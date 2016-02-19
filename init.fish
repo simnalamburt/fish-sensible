e48c2f35857aec03f07329366d042f08f286c32a
Hyeon Kim <simnalamburt@gmail.com>
2016-02-20 04:00:43 +0900

    Alias vim, vi -> nvim

--- init.fish
+++ init.fish
@@ -11,6 +11,12 @@ alias rd=rmdir
 alias mv='mv -i'
 alias cp='cp -i'
 
+# neovim
+if type -q nvim
+  alias vim=nvim
+  alias vi=nvim
+end
+
 # fasd
 if type -q fasd
   alias v='fasd -t -e vim -b viminfo'
